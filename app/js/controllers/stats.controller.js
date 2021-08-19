c5f74496d97516d0e47ac4aee978a822cecbc56d
Redrum <franco.avalos@live.com>
2021-08-19 13:24:05 -0400

    feat styles

--- app/js/controllers/stats.controller.js
+++ app/js/controllers/stats.controller.js
@@ -1,12 +1,29 @@
 'use strict';
 angular
     .module('app-search-repo')
-    .controller('StatsController', ['$scope', '$routeParams', 'SearchService', function($scope, $routeParams, SearchService) {
+    .controller('StatsController', ['$scope', '$routeParams', 'SearchService','$location', function($scope, $routeParams, SearchService, $location) {
         
         console.log('stats controller...');
         $scope.repository = $routeParams.query;
         $scope.dataStats = [];
         $scope.labelStats = [];
+        $scope.optionStats = {
+            labels: {
+                color: '#fffff'
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            }
+        };
 
         SearchService.getRepositories($scope.repository)
             .then((response) => {
@@ -21,6 +38,9 @@ angular
             }).catch((error) => {
                 window.alert('Upps ha ocurrido un problema: '+error.data.message);
             });
-
+        
+        $scope.goBack = () => {
+            $location.url('/');
+        };   
         
     }]);
